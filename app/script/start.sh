1876f5fc9a8fd712eb7ab45580f55bc3728e4f54
Dennis Bartlett <bartlett.dc.1@gmail.com>
2018-09-23 17:11:34 -0400

    Update start.sh

--- app/script/start.sh
+++ app/script/start.sh
@@ -27,13 +27,19 @@ if [ -z $THREAD ]; then
     THREAD=$(nproc)
 fi
 
+if [ -z $ALGO ]; then
+    echo "\$ALGO not found...... Default to Lyra2z"
+    ALGO=lyra2z
+fi
+
 echo
 echo "==================== SUMMARY ===================="
 echo "\$NH_REGION   : ${NH_REGION}"
 echo "\$BTC_ADDR    : ${BTC_ADDR}"
 echo "\$WORKER_NAME : ${WORKER_NAME}"
 echo "\$THREAD      : ${THREAD}"
+echo "\$ALGO        : ${ALGO}"
 echo
 echo "====================  START  ===================="
 
-./nheqminer_cpu_tromp -l "equihash.${NH_REGION}.nicehash.com:3357" -u "${BTC_ADDR}.${WORKER_NAME}" -t ${THREAD}
+./cpuminer -a ${ALGO} -o stratum+tcp://lyra2z.{$NH_REGION}.nicehash.com:3365 -u ${BTC_ADDR}.${WORKER_NAME} -t ${THREAD}
