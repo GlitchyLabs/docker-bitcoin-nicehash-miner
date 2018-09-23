69c90fcb0f9205c54cd27d7ccdc91c572a056070
Dennis Bartlett <bartlett.dc.1@gmail.com>
2018-09-23 17:14:20 -0400

    Update start.sh

--- app/script/start.sh
+++ app/script/start.sh
@@ -32,6 +32,11 @@ if [ -z $ALGO ]; then
     ALGO=lyra2z
 fi
 
+if [ -z $PORT ]; then
+    echo "\$PORT not found...... Default to 3365"
+    PORT=3365
+fi
+
 echo
 echo "==================== SUMMARY ===================="
 echo "\$NH_REGION   : ${NH_REGION}"
@@ -39,7 +44,8 @@ echo "\$BTC_ADDR    : ${BTC_ADDR}"
 echo "\$WORKER_NAME : ${WORKER_NAME}"
 echo "\$THREAD      : ${THREAD}"
 echo "\$ALGO        : ${ALGO}"
+echo "\$PORT        : ${PORT}"
 echo
 echo "====================  START  ===================="
 
-./cpuminer -a ${ALGO} -o stratum+tcp://lyra2z.{$NH_REGION}.nicehash.com:3365 -u ${BTC_ADDR}.${WORKER_NAME} -t ${THREAD}
+./cpuminer -a ${ALGO} -o stratum+tcp://${ALGO}.{$NH_REGION}.nicehash.com:${PORT} -u ${BTC_ADDR}.${WORKER_NAME} -t ${THREAD}
